06708dc2b411ecd3088aff6a763fc1b905e2831b
Ankur9669 <65125998+Ankur9669@users.noreply.github.com>
2022-05-30 21:38:45 +0530

    feat: completed explore page

--- fitbook1/src/components/centercontent/CenterContent.tsx
+++ fitbook1/src/components/centercontent/CenterContent.tsx
@@ -2,12 +2,16 @@ import Loader from "../../components/loader/Loader";
 import "./centercontent.css";
 import CreatePost from "./createpost/CreatePost";
 import Posts from "../../components/posts/Posts";
+import { useLocation } from "react-router";
+import { MdPendingActions } from "react-icons/md";
 
 const CenterContent = () => {
+  const location = useLocation();
+  const pathName = location.pathname;
   return (
     <div className="center-content">
       {/* <Loader /> */}
-      <CreatePost />
+      {pathName === "/" && <CreatePost />}
       <Posts />
     </div>
   );
--- fitbook1/src/pages/explore/Explore.tsx
+++ fitbook1/src/pages/explore/Explore.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import LeftSidebar from "../../components/left-sidebar/LeftSidebar";
 import RightSidebar from "../../components/right-sidebar/RightSidebar";
 import CreatePostModal from "../../components/createpostmodal/CreatePostModal";
+import CenterContent from "../../components/centercontent/CenterContent";
 
 const Explore = () => {
   const [isModalOpen, setModalOpen] = useState(false);
@@ -9,7 +10,7 @@ const Explore = () => {
     <>
       {isModalOpen && <CreatePostModal setModalOpen={setModalOpen} />}
       <LeftSidebar setModalOpen={setModalOpen} />
-      <div></div>
+      <CenterContent />
       <RightSidebar />
     </>
   );
