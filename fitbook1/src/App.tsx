5a3a1d7c54a8c1a348cdcd148ce2d1e25170dc43
Ankur Gupta <65125998+Ankur9669@users.noreply.github.com>
2022-06-02 16:59:24 +0530

    added redux and backend (#5)

--- fitbook1/src/App.tsx
+++ fitbook1/src/App.tsx
@@ -3,8 +3,19 @@ import Navbar from "./components/navbar/Navbar";
 import Homepage from "./pages/homepage/Homepage";
 import { Routes, Route } from "react-router-dom";
 import Explore from "./pages/explore/Explore";
+import { useEffect } from "react";
+import Axios from "axios";
 
 function App() {
+  useEffect(() => {
+    Axios.get("/api/posts")
+      .then((res) => {
+        console.log(res);
+      })
+      .catch(() => {
+        console.log("Error");
+      });
+  }, []);
   return (
     <div className="App">
       <Navbar />
