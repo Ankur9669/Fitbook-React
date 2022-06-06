13fcb945d0aa9bee2a7e3ca3bfd34eda39cb537a
Ankur9669 <65125998+Ankur9669@users.noreply.github.com>
2022-06-06 21:10:22 +0530

    fix: done with the changes suggested by peers

--- fitbook1/src/App.tsx
+++ fitbook1/src/App.tsx
@@ -5,22 +5,14 @@ import { Routes, Route } from "react-router-dom";
 import Explore from "./pages/explore/Explore";
 import Bookmark from "./pages/bookmark/Bookmark";
 import Profile from "./pages/profile/Profile";
-import { useEffect } from "react";
 import { ToastContainer } from "react-toastify";
 import Axios from "axios";
 import Login from "./pages/auth/login/Login";
 import SignUp from "./pages/auth/signup/SignUp";
+import { useDocumentTitle } from "./util/change-document-title";
 
 function App() {
-  useEffect(() => {
-    Axios.get("/api/posts")
-      .then((res) => {
-        console.log(res);
-      })
-      .catch(() => {
-        console.log("Error");
-      });
-  }, []);
+  useDocumentTitle("FitBook-Home");
   return (
     <div className="App">
       <ToastContainer />
