0730a1b26755789c6d8bac088ed27b04310d756e
Ankur9669 <65125998+Ankur9669@users.noreply.github.com>
2022-06-17 17:14:50 +0530

    feat: added colors to toast and changed the position of toasts

--- fitbook1/src/util/toasts/showToast.tsx
+++ fitbook1/src/util/toasts/showToast.tsx
@@ -1,14 +1,22 @@
+import { IoColorWandOutline } from "react-icons/io5";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+const toastConfiguration = {
+  position: toast.POSITION.BOTTOM_RIGHT,
+  autoClose: 800,
+  pauseOnHover: false,
+  hideProgressBar: true,
+  draggable: true,
+};
 export const showToast = (toastType: string, toastMessage: string) => {
   switch (toastType) {
     case "SUCCESS":
-      toast.success(toastMessage);
+      toast.success(toastMessage, toastConfiguration);
       break;
 
     case "ERROR":
-      toast.error(toastMessage);
+      toast.error(toastMessage, toastConfiguration);
       break;
 
     case "WARNING":
